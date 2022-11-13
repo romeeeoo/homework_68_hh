4c8000ac78efe23ca8c87f7e290dc459592125e6
Roman Tarassenko <tarassenko@me.com>
2022-11-13 16:33:42 +0300

    - зарегестрированна новая модель пользвателя в панели админа
     - исправлена ошибка: в urls.py главного приложения добавлен путь к папке uploads

--- hh/urls.py
+++ hh/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("auth/", include('accounts.urls')) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-]
+    path("auth/", include('accounts.urls'))
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
