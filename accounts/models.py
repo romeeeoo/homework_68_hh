b06da59c6f5f003f6ae97133adca52eecc2784a1
Roman Tarassenko <tarassenko@me.com>
2022-11-13 15:08:38 +0300

    Описана модель Profile расширяющая стандартную модель Юзера

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,22 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
+
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(
+        to=get_user_model(),
+        related_name="profile",
+        on_delete=models.CASCADE,
+        verbose_name="User profile"
+    )
+
+    avatar = models.ImageField(
+        null=True,
+        blank=True,
+        upload_to="profile_picture"
+    )
+    is_corporate = models.BooleanField(
+        null=False,
+        blank=False,
+    )
